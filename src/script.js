c6a15d89b3e390ef13dabed3c709789acea5ecc3
mr-mph <88854453+mr-mph@users.noreply.github.com>
2024-06-18 18:03:28 -0700

    change position of timer to be fixed

--- src/script.js
+++ src/script.js
@@ -15,8 +15,8 @@ window.addEventListener("load", () => {
         iframe = document.createElement("iframe");
         iframe.src = chrome.runtime.getURL("timer.html");
         iframe.style.position = "absolute";
-        iframe.style.top = "-30px";
-        iframe.style.right = "-70px";
+        iframe.style.top = "-10px";
+        iframe.style.right = "-50px";
         scale(iframe, 0.6, 200, 400);
         testContainer.append(iframe);
         setTimeout(() => {
